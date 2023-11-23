ff012b2f59765648e05a576c9beb41786e1914ea
attilagyoriid <attila.gyori.id@gmail.com>
2023-11-23 13:01:40 +0100

    Login feature skeleton

--- features/steps/loginSteps.ts
+++ features/steps/loginSteps.ts
@@ -1,5 +1,40 @@
-import { Given } from '@cucumber/cucumber';
+import { Given, Then, When } from '@cucumber/cucumber';
 
 Given('User navigates to the application', async function () {
+  return 'pending';
+});
+
+Given('User click on Sign in', async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('User at Customer Login view', async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('User enter username: {string}', async function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('User enter password: {string}', async function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('User click on login button', async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('Login should be success', async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('Login should fail', async function () {
+  // Write code here that turns the phrase above into concrete actions
   return 'pending';
 });
