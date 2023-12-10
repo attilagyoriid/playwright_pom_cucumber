2126c704836ece40a99671f123f242e859cd8911
attilagyoriid <attila.gyori.id@gmail.com>
2023-12-10 21:44:46 +0100

    Headless as cli option

--- framework/types/env.d.ts
+++ framework/types/env.d.ts
@@ -1,12 +1,13 @@
-export { };
+export {};
 
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            BROWSER: "chrome" | "firefox" | "webkit",
-            ENV: "staging" | "prod" | "test",
-            BASEURL: string,
-            HEAD: "true" | "false"
-        }
+  namespace NodeJS {
+    interface ProcessEnv {
+      BROWSER: 'chrome' | 'firefox' | 'webkit';
+      ENV: 'staging' | 'prod' | 'test';
+      BASEURL: string;
+      HEAD: 'true' | 'false';
+      HEADLESS: boolean;
     }
+  }
 }
