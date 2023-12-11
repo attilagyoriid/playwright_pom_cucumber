787e89b6644053bb55b39d66637acc22a0f8bed5
attilagyoriid <attila.gyori.id@gmail.com>
2023-12-11 13:09:11 +0100

    Extract locators and assertion

--- framework/pages/formLayoutsPage.ts
+++ framework/pages/formLayoutsPage.ts
@@ -1,20 +1,46 @@
-import { Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 import { BasePage } from './basePage';
 
 export class FormLayoutsPage extends BasePage {
+  readonly gridFormLocator: Locator;
+  readonly gridFormEmailInputLocator: Locator;
+  readonly girdFormPasswordInputLocator: Locator;
+  readonly girdFormRadioBtnLocator: Locator;
+  readonly girdFormSubmitBtnLocator: Locator;
+
+  readonly inlineFormLocator: Locator;
+  readonly inlineFormNameInputLocator: Locator;
+  readonly inlineFormEmailInputLocator: Locator;
+  readonly inlineFormRememberLocator: Locator;
+  readonly inlineFormSubmitBtnLocator: Locator;
+
+  readonly loginBtn: Locator;
+
   constructor(page: Page) {
     super(page);
+    // grid form locators
+    this.gridFormLocator = this.page.locator('nb-card', { hasText: 'Using the Grid' });
+    this.gridFormEmailInputLocator = this.gridFormLocator.getByRole('textbox', { name: 'Email' });
+    this.girdFormPasswordInputLocator = this.gridFormLocator.getByRole('textbox', { name: 'Password' });
+    this.girdFormRadioBtnLocator = this.gridFormLocator.locator('nb-radio-group');
+    this.girdFormSubmitBtnLocator = this.gridFormLocator.getByRole('button');
+
+    // basic form locators
+    this.inlineFormLocator = this.page.locator('nb-card', { hasText: 'Inline form' });
+    this.inlineFormNameInputLocator = this.inlineFormLocator.getByRole('textbox', { name: 'Jane Doe' });
+    this.inlineFormEmailInputLocator = this.gridFormLocator.getByRole('textbox', { name: 'Email' });
+    this.inlineFormRememberLocator = this.gridFormLocator.getByRole('checkbox');
+    this.inlineFormSubmitBtnLocator = this.gridFormLocator.getByRole('button');
   }
 
   async visitPage(path = '/') {
     await this.page.goto(path);
   }
   async submitUsingTheGrigdFormWithCredentialsAndSelectOption(email: string, password: string, optionText: string) {
-    const usingTheGridForm = this.page.locator('nb-card', { hasText: 'Using the Grid' });
-    await usingTheGridForm.getByRole('textbox', { name: 'Email' }).fill(email);
-    await usingTheGridForm.getByRole('textbox', { name: 'Password' }).fill(password);
-    await usingTheGridForm.getByRole('radio', { name: optionText }).check({ force: true });
-    await usingTheGridForm.getByRole('button').click();
+    await this.gridFormEmailInputLocator.fill(email);
+    await this.girdFormPasswordInputLocator.fill(password);
+    await this.girdFormRadioBtnLocator.locator(`:text-is("${optionText}")`).click({ force: true });
+    await this.girdFormSubmitBtnLocator.click();
   }
 
   /**
@@ -24,10 +50,9 @@ export class FormLayoutsPage extends BasePage {
    * @param rememberMe - true or false if user session to be safed
    */
   async sumbitInlineFormWithNameEmailAndCheckbox(name: string, email: string, rememberMe: boolean) {
-    const inlineForm = this.page.locator('nb-card', { hasText: 'Inline form' });
-    await inlineForm.getByRole('textbox', { name: 'Jane Doe' }).fill(name);
-    await inlineForm.getByRole('textbox', { name: 'Email' }).fill(email);
-    if (rememberMe) await inlineForm.getByRole('checkbox').check({ force: true });
-    await inlineForm.getByRole('button').click();
+    await this.inlineFormNameInputLocator.fill(name);
+    await this.inlineFormEmailInputLocator.fill(email);
+    if (rememberMe) await this.inlineFormRememberLocator.check({ force: true });
+    await this.inlineFormSubmitBtnLocator.click();
   }
 }
--- tests/testForms.spec.ts
+++ tests/testForms.spec.ts
@@ -1,11 +1,20 @@
+import { expect } from '@playwright/test';
 import { test } from '../test-options';
 
 test('form fill', async ({ pageManager }) => {
+  const expectedName = 'Attila Gyori';
+  const expectedPassword = 'Password';
+  const expectedOptionText = 'Option 2';
   await pageManager.formLayoutsPage.visitPage();
   await pageManager.sidebarComponent.expandFormLayoutsPage();
   await pageManager.formLayoutsPage.submitUsingTheGrigdFormWithCredentialsAndSelectOption(
-    'Attila Gyori',
-    'Password',
-    'Option 2'
+    expectedName,
+    expectedPassword,
+    expectedOptionText
   );
+  await expect(pageManager.formLayoutsPage.gridFormEmailInputLocator).toHaveValue(expectedName);
+  await expect(pageManager.formLayoutsPage.girdFormPasswordInputLocator).toHaveValue(expectedPassword);
+  await expect(
+    pageManager.formLayoutsPage.girdFormRadioBtnLocator.locator(`:text-is("${expectedOptionText}")`)
+  ).toBeChecked();
 });
